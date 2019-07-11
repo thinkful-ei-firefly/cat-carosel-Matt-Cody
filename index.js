56b32dfd41445c743040a82eb1d096c8b3af3309
Matthew Mbonu <mcmbonu1@gmail.com>
2019-07-11 12:21:55 -0400

    project done

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ $(".thumbnail").on("click", e => {
     alt: img.alt
   });
 });
+
+$(".thumbnail").on("keyup", e => {
+  const img = e.currentTarget.children[0];
+  console.log(img.src, img.alt);
+  $(".hero img").attr({
+    src: img.src,
+    alt: img.alt
+  });
+});
